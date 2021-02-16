0d52ce3dae55c894866d3cc9d3e28823fe4b939c
jetaehyun <jetaehyun0@gmail.com>
2021-02-15 22:04:06 -0500

    some heuristic stuff

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -36,7 +36,7 @@ class AlphaBetaAgent(agent.Agent):
             if found_min_node > max_node:
                 max_node = found_min_node
                 best_state = state
-            
+
         next_move = best_state[1]
         return next_move
 
@@ -64,3 +64,132 @@ class AlphaBetaAgent(agent.Agent):
             # Add board to list of successors
             succ.append((nb,col))
         return succ
+
+    # ---------------------------------------------------------------------------------
+    ##
+    # @Brief calculates the heuristic value of the board
+    #
+    # @Param brd board to be checked
+    #
+    # @return value of board
+    # ---------------------------------------------------------------------------------
+    def __heuristic(self, brd):
+        
+        # linear: 2n  -> 1: 2 | 2: 4 
+        # Square: n^2 -> 1: 1 | 2: 4
+
+        player = brd.player
+
+        for width in range(brd.w):
+            for height in range(brd.h):
+                
+                token = brd.board[width][height]
+
+                # if checking player
+                if token == player:
+
+                    for vertical in range(brd.n):
+
+                    for horizontal in range(brd.n):
+
+                    for diagonalPos in range(brd.n):
+
+                    for diagonalNeg in range(brd.n):
+
+
+    def __diagonalNeg(self, brd, x, y):
+        
+        points = 0
+        duplicates = 1
+        player = brd.player
+
+        for diag in range(brd.n):
+            
+            xPos = x + diag
+            yPos = y - diag
+
+            if xPos >= brd.w - 1 and yPos < 0:
+                break
+            
+            token = brd.board[xPos][yPos]
+            if token == player:
+                points += 2 * duplicates
+                duplicates += 1
+
+        return points
+
+
+    def __diagonalPos(self, brd, x, y):
+        
+        points = 0
+        duplicates = 1
+        player = brd.player
+
+        for diag in range(brd.n):
+
+            xPos = x + diag
+            yPos = y + diag
+
+            if xPos >= brd.w - 1 and yPos >= brd.h - 1:
+                break
+            
+            token = brd.board[x+diag][y+diag]
+            if token == player:
+                points += 2 * duplicates
+                duplicates += 1
+
+        return points
+
+    def __countVertical(self, brd, x, y):
+        
+        points = 0
+        duplicates = 1
+        player = brd.player
+
+        for vert in range(brd.n):
+            if vert >= brd.h - 1:
+                break
+            
+            token = brd.board[x][y+vert]
+            if token == player:
+                points += 2 * duplicates
+                duplicates += 1
+
+        return points
+
+    def __countHorizontal(self, brd, x, y):
+        points = 0
+        duplicates = 1
+        player = brd.player
+
+        for hor in range(brd.n):
+            if hor >= brd.w - 1:
+                break
+            
+            token = brd.board[x+hori][y]
+            if token == player:
+                points += 2 * duplicates
+                duplicates += 1
+
+        return points
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
