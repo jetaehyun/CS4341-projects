cd93a394694fed62cf6797d4c064ee718c7edfc9
jetaehyun <jetaehyun0@gmail.com>
2021-03-02 21:40:43 -0500

    A* implementation

--- Bomberman/group01/scenario1/variant1.py
+++ Bomberman/group01/scenario1/variant1.py
@@ -7,13 +7,13 @@ sys.path.insert(1, '..')
 from game import Game
 
 # TODO This is your code!
-sys.path.insert(1, '../groupNN')
+sys.path.insert(1, '../group01')
 
 # Uncomment this if you want the empty test character
-#from testcharacter import TestCharacter
+from testcharacter import TestCharacter
 
 # Uncomment this if you want the interactive character
-from interactivecharacter import InteractiveCharacter
+# from interactivecharacter import InteractiveCharacter
 
 # Create the game
 g = Game.fromfile('map.txt')
@@ -21,21 +21,22 @@ g = Game.fromfile('map.txt')
 # TODO Add your character
 
 # Uncomment this if you want the test character
-# g.add_character(TestCharacter("me", # name
-#                               "C",  # avatar
-#                               0, 0  # position
-# ))
+g.add_character(TestCharacter("me", # name
+                              "C",  # avatar
+                              0, 0,  # position
+                              0
+))
 
 # Uncomment this if you want the interactive character
-g.add_character(InteractiveCharacter("me", # name
-                                     "C",  # avatar
-                                     0, 0  # position
-))
+# g.add_character(InteractiveCharacter("me", # name
+#                                      "C",  # avatar
+#                                      0, 0  # position
+# ))
 
 # Run!
 
 # Use this if you want to press ENTER to continue at each step
-# g.go(0)
+#g.go(0)
 
 # Use this if you want to proceed automatically
 g.go(1)
