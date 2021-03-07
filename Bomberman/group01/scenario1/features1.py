e2a13b549958088411dc0217fa519d0c9ec34ec5
jetaehyun <jetaehyun0@gmail.com>
2021-03-07 16:29:28 -0500

    features1 script barebones

---
+++ Bomberman/group01/scenario1/features1.py
@@ -0,0 +1,16 @@
+
+
+def distToMonster(character, monster):
+    return 2
+
+def distToExit(character, exit):
+    return 10
+
+def distToBomb(character, bomb):
+    return 1
+
+def distToWall(character, wrld):
+    return 5
+
+def bombDropped():
+    return True
