46f49243c626052429b1507eb06e594b3f63abf3
jetaehyun <jetaehyun0@gmail.com>
2021-03-08 20:00:00 -0500

    distance to entity function

--- Bomberman/group01/scenario1/features1.py
+++ Bomberman/group01/scenario1/features1.py
@@ -1,23 +1,53 @@
-from patrick_star import perform_aStar
+from patrick_star import perform_aStar, _heuristic
+
+# --------------------------------------------------------------------------
+##
+# @Brief find the nearest entity to the character
+#
+# @Param wrld world object
+# @Param character position of character (x,y)
+# @Param entity desired entity to find
+#
+# @Returns None: nothing found, list(): of positions
+# --------------------------------------------------------------------------
+def findNearestEntity(wrld, character, entity):
+    x,y = character
+    width,height = wrld.width(), wrld.height()
+    
+    distanceTo = 99999
+    coord = None
+
+    for w in range(width):
+        for h in range(height):
+            distanceToFound = 99999
+            if entity == "monster":
+                if wrld.monsters_at(w, h) != None:
+                    distanceToFound = _heuristic((w,h), (x,y))
+
+            elif entity == "bomb":
+                if wrld.bomb_at(w,h) != None:
+                    distanceToFound = _heuristic((w,h), (x,y))
+
+            elif entity == "exit":
+                if wrld.exit_at(w,h):
+                    return (w,h)
+
+            if distanceToFound<distanceTo:
+                distanceTo = distanceToFound
+                coord = (w,h)
+    return coord
 
 
-def distToMonster(wrld, character):
-    return 0.4
+def distToEntity(wrld, character, entity):
+    entityPosition = findNearestEntity(wrld, character, entity)
 
-def distToExit(wrld, character):
-
-    path = perform_aStar(wrld, character, True)
-
-    if path == None:
+    if entityPosition == None:
         return 0
 
-    return 1 / (1 + path)**2
+    path = perform_aStar(wrld, character, entityPosition, True)
+    distance = len(path)
+    return 1 / (1+distance)**2
 
-def distToBomb(wrld, character):
-    return 0.8
-
-def distToWall(wlrd, character):
-    return 0.4
 
 def inBombExplosionRange(wrld, character):
    
