abc399503e0195c732523406f7a0f51a127cde99
jetaehyun <jetaehyun0@gmail.com>
2021-03-19 12:28:42 -0400

    changed variant1 constructor

--- Bomberman/group01/scenario2/variant1.py
+++ Bomberman/group01/scenario2/variant1.py
@@ -9,25 +9,16 @@ from game import Game
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
 
-from QLearner import QLearner
-from QCharacter import QCharacter
-from features import *
+from StateCharacter1 import StateCharacter1
 
 # Create the game
 g = Game.fromfile('map.txt')
-features = [distanceToExit, distanceToBomb, inBombExplosionRange, anyDroppedBombs]
 
-weights = [114.69554463857256, -2.861841880284248,  -6.044985197169929, 8.175889714036572]
-
-qlearner = QLearner(weights, features)
-prev_wrld = None
 # TODO Add your character
-q_character = QCharacter("me", # name
+q_character = StateCharacter1("me", # name
                                "C",  # avatar
                                0, 0,  # position
-                               qlearner,
-							   False,
-                               1)
+                                )
 
 	#Uncomment this if you want the interactive character
 g.add_character(q_character)
